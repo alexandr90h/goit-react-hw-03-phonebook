5056dc814c832a12b97d3998cc94a7246f02e64b
Alexandr <63474923+alexandr90h@users.noreply.github.com>
2020-12-13 21:23:19 +0200

    13.12.2020-1

---
+++ src/contactsList/contactsItem.jsx
@@ -0,0 +1,12 @@
+import styles from './contactsItem.module.scss';
+export default function ContactsList({ id, name, number, onBtnDelId }) {
+    return (
+        <li key={id} className={styles.contactItem}>
+            <div className={styles.contentItem}>
+                <span className={styles.nameItem}>{name}: </span>
+                <span className={styles.phonItem}>{number}</span>
+            </div>
+            <button onClick={() => onBtnDelId(id)}>delete</button>
+        </li>
+    )
+}
